c8c9a633dfb1908a6f9c572a41e1c5d9656284dd
Esther Fuhrmann <esther.fuhrmann@arxes-tolina.de>
2018-12-11 15:16:58 +0100

    test gefixt

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,7 +10,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class DemoApplicationTests {
 
   @Test
-  public void contextLoads() {
-    fail();
-  }
+  public void contextLoads() {}
 }
